858197f0c788aca24ce0bead684a4bfea5e5c786
giampaolo <giampaorr@gmail.com>
2025-05-21 22:14:38 +0200

    insert services

---
+++ audit-log-service/src/main/java/com/bank/system/audit_log_service/AuditLogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.system.audit_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditLogServiceApplication.class, args);
+	}
+
+}
---
+++ ui-service/src/test/java/com/bank/system/ui_service/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.system.ui_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
